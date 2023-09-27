5fcd7b62970780b823010b6e2b9403b4cbe91ae0
Simon H <professional.simonhossain@gmail.com>
2023-09-27 23:09:39 +0100

    Composing running java code

--- two_sum.java
+++ two_sum.java
@@ -1,1 +1,11 @@
-fidi
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(i=0; i < nums.length; i++) {
+            for(k=0; k < nums.length - 1; k++) {
+                if (nums[i] + nums[k+1] = target) {
+                    return int[] [i,k+1]
+                }
+            }
+        }
+    }
+}
