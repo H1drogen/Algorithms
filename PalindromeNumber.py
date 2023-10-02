982342480b76d95278bcc1374ffa1da7e3595e54
Simon H <professional.simonhossain@gmail.com>
2023-10-02 21:50:41 +0100

    Palindrome solved

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,18 +1,18 @@
 #Given an integer x, return true if x is a palindrome, and false otherwise.
 
 class Solution(object):
-    def isPalindrome(self, x):
+    def isPalindrome(self, x: int):
         if 10 > x > 0:
             return True
         if x > 10:
-            while x != 0:
-                last_digit = x % 10
+            mockx = x
+            reversed = 0
+            while int(mockx) != 0:
+                last_digit = int(mockx) % 10
+
                 reversed = (reversed * 10) + last_digit
-                x /= 10
-            if reversed == x:
-                return True
-        else:
-            return False
+                mockx /= 10
+        return bool(reversed == x)
 
 
 
---
+++ main.py
@@ -0,0 +1,4 @@
+import PalindromeNumber
+
+solution = PalindromeNumber.Solution()
+print(solution.isPalindrome(222))
