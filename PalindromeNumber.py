6f1d2f4cf1d1e4f29b9710d68e93e514ac5610ea
Simon H <professional.simonhossain@gmail.com>
2023-10-02 22:10:08 +0100

    Palindrome Fixes

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -2,14 +2,13 @@
 
 class Solution(object):
     def isPalindrome(self, x: int):
+        reversed = 0
         if 10 > x > 0:
             return True
         if x > 10:
             mockx = x
-            reversed = 0
             while int(mockx) != 0:
                 last_digit = int(mockx) % 10
-
                 reversed = (reversed * 10) + last_digit
                 mockx /= 10
         return bool(reversed == x)
