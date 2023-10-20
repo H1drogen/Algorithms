f11988e8d814e9cf93fc2fba4c55ffb65366f64b
Simon H <professional.simonhossain@gmail.com>
2023-10-20 14:29:55 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 def test(arr):
     positive = 0
     negative = 0
@@ -9,4 +12,19 @@ def test(arr):
             negative += 1
         else:
             zero += 1
+
+def cookies(k, A):
+    iterations = 0
+    x = 0
+    heapq.heapify(A)
+    while A[0] < k and len(A) > 1:
+        temp = heapq.heappop(A)
+        new_cookie = temp + 2*A[0]
+        heapq.heapreplace(A, new_cookie)
+        iterations += 1
+    if A[-1] < k:
+        iterations = -1
+    return iterations
+
+cookies(9, [2,7,3,6,4,6])
 
