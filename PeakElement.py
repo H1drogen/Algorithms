fa5cd11704292d23de0840d5498a1961d259bb8b
Simon H <professional.simonhossain@gmail.com>
2024-02-05 17:21:31 +0000

    Update PeakElement.py

--- PeakElement.py
+++ PeakElement.py
@@ -6,10 +6,11 @@ class Solution(object):
             return 0
         if nums[x] < nums[x-1]:
             return 0
-        while x < len(nums):
+        while x < len(nums) - 1:
             if nums[x] > nums[x+1]:
                 if nums[x] > nums[x-1]:
                     return x
             x += 1
+        return x
 
 Solution().findPeakElement([1,2,3,1])
