ac979726568b61dd031569ba1a02df4fe7a87286
Simon H <professional.simonhossain@gmail.com>
2024-02-05 12:25:06 +0000

    Create PeakElement.py

---
+++ PeakElement.py
@@ -0,0 +1,15 @@
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        x = 1
+        if len(nums) <= 1:
+            return 0
+        if nums[x] < nums[x-1]:
+            return 0
+        while x < len(nums):
+            if nums[x] > nums[x+1]:
+                if nums[x] > nums[x-1]:
+                    return x
+            x += 1
+
+Solution().findPeakElement([1,2,3,1])
