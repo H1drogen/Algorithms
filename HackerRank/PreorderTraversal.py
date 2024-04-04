98b2c59b67c13306a1351aa577e07083ada1e34c
SimonH <professional.simonhossain@gmail.com>
2024-04-04 11:56:40 +0100

    PreOrder binary tree traversal solved

---
+++ HackerRank/PreorderTraversal.py
@@ -0,0 +1,26 @@
+from data_structures import binary_tree
+
+def preOrder(root):
+    values = []
+    traversal(root, values)
+    print(" ".join(str(x) for x in values))
+
+def traversal(root, data):
+    if root is not None:
+        data.append(root.info)
+        traversal(root.left, data)
+        traversal(root.right, data)
+
+
+
+
+
+
+root = binary_tree.Node(1)
+root.right = binary_tree.Node(2)
+root.right.right = binary_tree.Node(5)
+root.right.right.right = binary_tree.Node(3)
+root.right.right.left = binary_tree.Node(6)
+root.right.right.right.right = binary_tree.Node(4)
+
+preOrder(root)
