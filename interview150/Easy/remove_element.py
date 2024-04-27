201580487e03b19dfc350e46d37d19474cb5c424
SimonH <professional.simonhossain@gmail.com>
2024-04-27 23:36:58 +0100

    solved remove element algorithm

--- interview150/Easy/remove_element.py
+++ interview150/Easy/remove_element.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        count = 0
+        index = 0
+        while index < len(nums):
+            if nums[index] == val:
+                nums.pop(index)
+                count += 1
+            else:
+                index += 1
+        return count
+
+
+
--- interview150/tests.py
+++ interview150/tests.py
@@ -1,21 +1,26 @@
 import unittest
-from interview150.Easy.merge_sorted_array import Solution
+from interview150.Easy.merge_sorted_array import Solution as MergeSolution
+from interview150.Easy.remove_element import Solution as RemoveElementSolution
 
 class TestSolutions(unittest.TestCase):
     def setUp(self):
-        self.solution = Solution()
+        self.merge_solution = MergeSolution()
+        self.remove_element_solution = RemoveElementSolution()
 
     def test_merge_sorted_array(self):
         nums1 = [1, 2, 3, 0, 0, 0]
         m = 3
         nums2 = [2, 5, 6]
         n = 3
-        self.solution.merge(nums1, m, nums2, n)
+        self.merge_solution.merge(nums1, m, nums2, n)
         self.assertEqual(nums1, [1, 2, 2, 3, 5, 6])
+
+    def test_removeElement(self):
+        self.assertEqual(self.remove_element_solution.removeElement([3, 2, 2, 3], 3), 2)
+        self.assertEqual(self.remove_element_solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2), 3)
 
 
 if __name__ == '__main__':
     unittest.main()
-
 
 
