2c7377ae32503fc50c663dc8ac9ddb6207a43d99
SimonH <professional.simonhossain@gmail.com>
2024-05-15 23:36:07 +0100

    answer

--- interview150/Easy/majority_element.py
+++ interview150/Easy/majority_element.py
@@ -6,3 +6,19 @@ from typing import List
 def majorityElement(nums: List[int]) -> int:
     nums.sort()
     return nums[int(len(nums) / 2)]
+
+
+
+# ANSWER
+# it can be optimized to O(n) using the Boyer-Moore Voting Algorithm
+
+def majorityElement(nums: List[int]) -> int:
+    count = 0
+    candidate = None
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+
+    return candidate
