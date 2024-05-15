fb589e73895c6a4a9698d6425aa5c8a4b73e5316
SimonH <professional.simonhossain@gmail.com>
2024-05-15 23:34:07 +0100

    First attempt solution

--- interview150/Easy/majority_element.py
+++ interview150/Easy/majority_element.py
@@ -1,2 +1,8 @@
 # Given an array nums of size n, return the majority element.
 # The majority element is the element that appears more than ⌊n / 2⌋ times.
+from typing import List
+
+
+def majorityElement(nums: List[int]) -> int:
+    nums.sort()
+    return nums[int(len(nums) / 2)]
