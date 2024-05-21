17d1244f04629e1a1c9a5a05b47a40a73e72dd75
Simon H. <professional.simonhossain@gmail.com>
2024-05-21 22:14:32 +0100

    Answer to rotate_array

--- interview150/medium/rotate_array.py
+++ interview150/medium/rotate_array.py
@@ -6,4 +6,10 @@ def rotate(self, nums: List[int], k: int) -> None:
     for n in range(k):
         nums.insert(0, nums.pop(len(nums) - 1))
 
-        
+# pop and insert have time complexity O(n). This is complexity O(n*k)
+
+# use slicing, which has a time complexity of O(k).
+
+def rotate(nums: List[int], k: int) -> None:
+    k %= len(nums)  # Ensure k is within the range of the list length
+    nums[:] = nums[-k:] + nums[:-k]
