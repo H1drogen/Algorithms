5928ea9221359416a76892a52620d69b226023a6
SimonH <professional.simonhossain@gmail.com>
2024-12-19 22:03:13 +0000

    word pattern problem

---
+++ LeetcodePython/Easy/word_pattern.py
@@ -0,0 +1,20 @@
+
+def wordPattern(pattern: str, s: str) -> bool:
+    hashmap = {}
+    words = s.split()
+    if len(words) != len(pattern):
+        return False
+    for i in range(len(pattern)):
+        if words[i] not in hashmap.values():
+            if pattern[i] in hashmap.keys():
+                return False
+            hashmap[pattern[i]] = words[i]
+        elif words[i] == hashmap[pattern[i]]:
+            continue
+        else:
+            return False
+    return True
+
+wordPattern('abba', 'dog cat cat dog')
+
+
