dd2da1e736231a1d693fd975f49db88ac25f59df
SimonH <professional.simonhossain@gmail.com>
2024-12-20 23:54:51 +0000

    Create InvertBinaryTree.py

---
+++ LeetcodePython/Binary_Trees/easy/InvertBinaryTree.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from LeetcodePython.Binary_Trees.treenode import TreeNode
+
+
+def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if root is null:
+        return null
+
+
+    return
+
+invertTree(TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9))))
+# Expected: TreeNode(4, TreeNode(7, TreeNode(9), TreeNode(6)), TreeNode(2, TreeNode(3), TreeNode(1)))
