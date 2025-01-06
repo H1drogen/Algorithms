f57be416e4a709375582c9ec59b65d1009cda668
SimonH <professional.simonhossain@gmail.com>
2025-01-06 23:21:59 +0000

    phone number solved

--- LeetcodePython/Medium/LetterCombinationsofPhoneNumber.py
+++ LeetcodePython/Medium/LetterCombinationsofPhoneNumber.py
@@ -4,3 +4,26 @@ from typing import List
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 def letterCombinations(digits: str) -> List[str]:
+    if not digits:
+        return []
+    mapping = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+    result = []
+    def backtrack(index, path):
+        if index == len(digits):
+            result.append(''.join(path))
+            return
+        for letter in mapping[digits[index]]:
+            path.append(letter)
+            backtrack(index + 1, path)
+            path.pop()
+    backtrack(0, [])
+    return result
