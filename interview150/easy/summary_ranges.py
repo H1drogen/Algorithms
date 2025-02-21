7936fb358f69e756f0d8c19263aac0a237358385
SimonH <professional.simonhossain@gmail.com>
2025-02-21 19:26:55 +0000

    refactoring + summary ranges

---
+++ interview150/easy/summary_ranges.py
@@ -0,0 +1,7 @@
+from typing import List
+
+# You are given a sorted unique integer array nums.
+# A range [a,b] is the set of all integers from a to b (inclusive).
+# Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+def summaryRanges(self, nums: List[int]) -> List[str]:
