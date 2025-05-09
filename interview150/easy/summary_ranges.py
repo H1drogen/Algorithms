930988e171c19ac4727920aa619c044f939ca4a6
SimonH <professional.simonhossain@gmail.com>
2025-05-09 23:04:42 +0100

    first attempt solution

--- interview150/easy/summary_ranges.py
+++ interview150/easy/summary_ranges.py
@@ -4,4 +4,18 @@ from typing import List
 # A range [a,b] is the set of all integers from a to b (inclusive).
 # Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
 
-def summaryRanges(self, nums: List[int]) -> List[str]:
+def summaryRanges(nums: List[int]) -> List[str]:
+    ranges = []
+    start = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1] + 1:
+            if start == nums[i - 1]:
+                ranges.append(str(start))
+            else:
+                ranges.append(f"{start}->{nums[i - 1]}")
+            start = nums[i]
+    if start == nums[-1]:
+        ranges.append(str(start))
+    else:
+        ranges.append(f"{start}->{nums[-1]}")
+    return ranges
